6b59e3e97927cc5492fc9b5007213b1c999d92d4
HamzaSx <HamzaSxH@gmail.com>
2023-01-08 10:36:40 +0300

    I removed the text border because it didn't do anything.

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='relative [*]:border-2 [*]:border-primary overflow-x-hidden text-white'>
+      <main className='relative overflow-x-hidden text-white'>
         <Navbar />
         <Hero />
         <About />
